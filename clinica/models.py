c0342e690efdbe8df31f3ff69c3d6088ca968959
gaSombrio <sombrio.ga@gmail.com>
2025-07-04 14:14:29 -0300

    FEAT: criação da model paciente #5

---
+++ clinica/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+class Especialidade(Models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+    
+class Medico(Models.model):
+    nome = models.CharField(max_length=100)
+    crm = models.CharField(max_length=6, unique=True)
+    telefone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=100)
+    especialidade = models.ForeignKey(Especialidade, on_delete=models.CASCADE, related_name='medicos')
+
+    def __str__(self):
+        return self.nome
+
+class Paciente(Models.model):
+    nome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=11, unique=True)
+    telefone = models.CharField(max_length=20)
+    data_nascimento = models.DateField()
